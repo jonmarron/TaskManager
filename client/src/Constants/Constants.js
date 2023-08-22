d5f84135ecf88e469322a1c0e2dfdfc5fa467913
jon <ldegjon@gmail.com>
2023-08-22 13:58:36 +0200

    frontend: added a new endpoint to the constants

--- client/src/Constants/Constants.js
+++ client/src/Constants/Constants.js
@@ -2,5 +2,6 @@ export const constants = {
   baseURL:'http://localhost:8080',
   projects: '/projects',
   clients: '/clients',
-  status: '/status'
+  status: '/status',
+  projectTypes: '/projectTypes'
 }
