7fc6cb931229a2b4d9077ced6aa7be6374adf706
jon <ldegjon@gmail.com>
2023-08-23 13:25:08 +0200

    backend: added JWT

--- server/src/main/java/com/jontxu/TaskManager/security/SecurityConfiguration.java
+++ server/src/main/java/com/jontxu/TaskManager/security/SecurityConfiguration.java
@@ -1,73 +1,98 @@
 package com.jontxu.TaskManager.security;
 
 import com.jontxu.TaskManager.model.UserRepository;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(
         securedEnabled = true
 )
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class SecurityConfiguration {
 
     //    Filter chain configuration
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.cors(Customizer.withDefaults())
+        return http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/projects").hasRole("USER")
-                        .requestMatchers("/clients").hasRole("ADMIN")
+                        .requestMatchers("/projects").hasAuthority("ADMIN")
+                        .requestMatchers("/clients").permitAll()
                         .requestMatchers("/db").permitAll()
                         .requestMatchers("/status").permitAll()
                         .requestMatchers("/projectTypes").permitAll()
                         .anyRequest().authenticated()
                 )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
-                .headers(o -> o.frameOptions().disable())
+                .headers(o -> o.frameOptions().disable()) // this lets H2 database platform work
                 .build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:5173"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
     //    UserDetailService
-    /*@Bean
+    @Bean
     UserDetailsService userDetailsService(UserRepository userRepository) {
         return username -> userRepository.findByUsername(username)
                 .map(UserPrincipal::new)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
-    }*/
-
-    @Bean
-    UserDetailsService inMemoryUserDetailsManager() {
-        UserDetails user = User.builder().username("user")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder().username("admin")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER","ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
     }
-
 
     //    password encoder
     @Bean
     PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    @Bean
+    JwtDecoder jwtDecoder(RsaKeyProperties properties){
+        return NimbusJwtDecoder.withPublicKey(properties.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(RsaKeyProperties properties){
+        JWK jwk = new RSAKey.Builder(properties.publicKey()).privateKey(properties.privateKey()).build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwkSource);
     }
 }
