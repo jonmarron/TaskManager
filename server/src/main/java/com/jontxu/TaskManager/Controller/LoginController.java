2e1bfbb72a1d2872609c7ef31cf4c26f39835013
jon <ldegjon@gmail.com>
2023-09-06 12:39:55 +0200

    backend: set the login endpoint to return both the JWT token and the authorities

--- server/src/main/java/com/jontxu/TaskManager/Controller/LoginController.java
+++ server/src/main/java/com/jontxu/TaskManager/Controller/LoginController.java
@@ -1,10 +1,17 @@
 package com.jontxu.TaskManager.Controller;
 
 import com.jontxu.TaskManager.logic.JwtGenerator;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("login")
@@ -16,7 +23,15 @@ public class LoginController {
     }
 
     @GetMapping
-    String jwt(Authentication authentication) {
-        return jwtGenerator.generate(authentication);
+    Map<String, Object> jwt(Authentication authentication) {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Collection<String> authorityNames = authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        String jwtToken = jwtGenerator.generate(authentication);
+        Map<String, Object> response = new HashMap<>();
+        response.put("jwtToken", jwtToken);
+        response.put("authorities", authorityNames);
+        return response;
     }
 }
